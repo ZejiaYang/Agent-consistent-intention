e8550faa52b602da2a43ce9204a96bd6e10a25b8
Grace Colverd <gb669@cam.ac.uk>
2024-01-08 12:10:53 +0000

    update fn with correct paths etc and timer

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,5 +7,6 @@ from examples_calls import *
 __all__  =  (load_data, json_arr_to_file, convert_statement, get_response_text, run_api_call
              , preprocess_options_and_labels, intention_prompt_first, intention_prompt_second
              , prompt_dataset
-             , load_examples_h, load_examples_all
+             , load_examples_h, load_examples_all 
+             ,timer 
                )
