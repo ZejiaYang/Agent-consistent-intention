874aa1408f8d8a54ff98c61d792f4ccb4e905539
Grace Colverd <gb669@cam.ac.uk>
2024-01-08 11:20:28 +0000

    add new fiels to innit

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,4 +4,5 @@ from prompt_fns import *
 
 __all__  =  (load_data, json_arr_to_file, convert_statement, get_response_text, run_api_call
              , preprocess_options_and_labels, intention_prompt_first, intention_prompt_second
+             , prompt_dataset
                )
