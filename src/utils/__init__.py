76eac64cb8eabcf08d5c47a5b5b0bbc38cfa6a97
Grace Colverd <gb669@cam.ac.uk>
2024-01-25 22:58:56 +0000

    update utils for 5 prong examples and custom adaptive

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,9 @@
 from json_proc import *
 from model_api.openai_api_calls import run_api_call
-from utils.model_api.hf_api_calls import *
+# from utils.model_api.hf_api_calls import *
 from prompt_fns import *
 from examples_calls import * 
+from .analysis_fns import *
 
 
 __all__  =  (load_data, json_arr_to_file
@@ -13,9 +14,13 @@ __all__  =  (load_data, json_arr_to_file
             , intention_prompt_second
             # ,prompt_dataset
             ,run_api_call
-            ,run_hf_api_call
+            # ,run_hf_api_call
             , load_examples_h 
             #  load_examples_all 
              ,timer 
              ,prompt_dataset_5pin
+             , create_plot_fivepin
+             , process_data
+             , load__concat_data_fivepin
+             , create_stack_bar_5pin
                )
